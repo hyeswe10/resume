aef6ce961cfd0548016be93c1062291b8524e8f4
jen-lap <jmoon981107@gmail.com>
2026-06-19 13:55:35 +0900

    헤더 틀 완료(버튼링크연결필요)

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { MdOutlineEmail } from "react-icons/md";
+import { CiPhone } from "react-icons/ci";
+import { IoIosLink } from "react-icons/io";
+import { GoArrowUpRight } from "react-icons/go";
+
+const Header = () => {
+    return (
+        <div id='header'>
+            <div className='left'>
+                <div className='img'></div>
+                <div className='info'>
+                    <div className='top'>
+                        <h1>문정우</h1>
+                        <p className='bold'>FRONTEND DEVELOPER</p>
+                        <p>근본 원인을 찾아 개선하는 프론트엔드 개발자</p>
+                    </div>
+                    <div className='bottom'>
+                        <div className='email'>
+                            <MdOutlineEmail />
+                            <p>krihel1107@gmail.com</p>
+                        </div>
+                        <div className='github'>
+                            <IoIosLink />
+                            <p>github.com/hyeswe10</p>
+                        </div>
+                        <div className='phone'>
+                            <CiPhone />
+                            <p>010-7365-1605</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='right'>
+                <a href='https://hyeswe10.github.io/mportfolio' target='_blank' rel='noopener noreferrer' className='portfolio'>포트폴리오<GoArrowUpRight /></a>
+                <a href='' className='cover'>자기소개서<GoArrowUpRight /></a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
