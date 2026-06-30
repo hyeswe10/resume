25717fbe82c38783f094beab795a1caff79f4fc0
hyeswe10 <jmoon981107@gmail.com>
2026-06-30 12:33:52 +0900

    keyword 버튼 완료

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import "./App.scss";
+import Keyword from './components/Keyword';
+import MainInfo from './components/MainInfo';
 
 const App = () => {
   return (
     <div id='app'>
       <Header/>
+      <Keyword/>
+      <MainInfo/>
     </div>
   );
 };
---
+++ src/components/Keyword.js
@@ -0,0 +1,33 @@
+
+const Keyword = () => {
+    return (
+        <div id="keyword">
+            <div className="fast">
+                <div className="line"></div>
+                <h3>빠른 적응력</h3>
+                <div>
+                    <p>신기술 습득</p>
+                    <p>능동적</p>
+                </div>
+            </div>
+            <div className="com">
+                <div className="line"></div>
+                <h3>원활한 소통</h3>
+                <div>
+                    <p>팀 협업</p>
+                    <p>피드백 수용</p>
+                </div>
+            </div>
+            <div className="study">
+                <div className="line"></div>
+                <h3>꾸준한 학습</h3>
+                <div>
+                    <p>TS 학습</p>
+                    <p>알고리즘</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Keyword;
